7165a960d1e1e1ddc31bcd0210e5c84412996369
里安 <lienyu@brmyx.com>
2025-07-31 11:50:17 +0800

    完成独立视角控制

--- Assets/Script/MainGame/CustomInputHandler.cs
+++ Assets/Script/MainGame/CustomInputHandler.cs
@@ -3,25 +3,26 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using Unity.Cinemachine;
 
+
 // This class receives input from a PlayerInput component and disptaches it
 // to the appropriate Cinemachine InputAxis.  The playerInput component should
 // be on the same GameObject, or specified in the PlayerInput field.
 class CustomInputHandler : InputAxisControllerBase<CustomInputHandler.Reader>
 {
     [Header("Input Source Override")]
-    public PlayerInput PlayerInput;
+    private PlayerInput playerInput;
 
     void Awake()
     {
         // When the PlayerInput receives an input, send it to all the controllers
-        if (PlayerInput == null)
-            TryGetComponent(out PlayerInput);
-        if (PlayerInput == null)
+        Transform playerControllerObj = transform.parent; // PlayerController
+        playerInput = playerControllerObj.GetComponent<PlayerInput>();
+        if (playerInput == null)
             Debug.LogError("Cannot find PlayerInput component");
         else
         {
-            PlayerInput.notificationBehavior = PlayerNotifications.InvokeCSharpEvents;
-            PlayerInput.onActionTriggered += (value) =>
+            playerInput.notificationBehavior = PlayerNotifications.InvokeCSharpEvents;
+            playerInput.onActionTriggered += (value) =>
             {
                 for (var i = 0; i < Controllers.Count; i++)
                     Controllers[i].Input.ProcessInput(value.action);
@@ -41,6 +42,7 @@ class CustomInputHandler : InputAxisControllerBase<CustomInputHandler.Reader>
     public class Reader : IInputAxisReader
     {
         public InputActionReference Input;
+        public float Sensitivity = 1.0f; // 灵敏度调节变量，默认为1.0，可在Inspector中调整
         Vector2 m_Value; // the cached value of the input
 
         public void ProcessInput(InputAction action)
@@ -58,7 +60,8 @@ class CustomInputHandler : InputAxisControllerBase<CustomInputHandler.Reader>
         // IInputAxisReader interface: Called by the framework to read the input value
         public float GetValue(UnityEngine.Object context, IInputAxisOwner.AxisDescriptor.Hints hint)
         {
-            return (hint == IInputAxisOwner.AxisDescriptor.Hints.Y ? m_Value.y : m_Value.x);
+            // 乘以灵敏度
+            return (hint == IInputAxisOwner.AxisDescriptor.Hints.Y ? m_Value.y : m_Value.x) * Sensitivity;
         }
     }
 }
